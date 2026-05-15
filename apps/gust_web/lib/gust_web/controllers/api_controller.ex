4b4aba0bf60ad39d00b3f2fb3a2bcadabb689c69
Karina Almeida <s.karinaalmeida@gmail.com>
2026-05-15 15:35:23 -0300

    create with satus enqueued and use if instead of case for binary

--- apps/gust_web/lib/gust_web/controllers/api_controller.ex
+++ apps/gust_web/lib/gust_web/controllers/api_controller.ex
@@ -1,25 +1,24 @@
 defmodule GustWeb.APIController do
   use GustWeb, :controller
 
-  alias Gust.DAG.Run.Trigger
   alias Gust.Flows
 
   plug(GustWeb.Plugs.APIAuth)
 
   def create_run(conn, %{"dag_name" => dag_name}) do
-    case Flows.get_dag_by_name(dag_name) do
-      nil ->
-        conn
-        |> put_status(:not_found)
-        |> json(%{error: "dag_not_found"})
+    dag = Flows.get_dag_by_name(dag_name)
 
-      dag ->
-        {:ok, run} = Flows.create_run(%{dag_id: dag.id})
-        run = Trigger.dispatch_run(run)
+    {status, payload} =
+      if dag do
+        {:ok, run} = Flows.create_run(%{dag_id: dag.id, status: :enqueued})
 
-        conn
-        |> put_status(:created)
-        |> json(%{id: to_string(run.id)})
-    end
+        {:created, %{id: to_string(run.id), status: to_string(run.status)}}
+      else
+        {:not_found, %{error: "dag_not_found"}}
+      end
+
+    conn
+    |> put_status(status)
+    |> json(payload)
   end
 end
